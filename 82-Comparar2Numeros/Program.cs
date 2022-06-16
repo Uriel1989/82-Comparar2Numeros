24771bd0e0daa7727b4c7dbebff0a2bf004d1e94
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:50:18 -0300

    Add files via upload

---
+++ 82-Comparar2Numeros/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*Pedir dos números al usuario por teclado y decir que número es el mayor.
+ */
+namespace _82_Comparar2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese 2 numeros para determinar cual es el mayor");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            int b = Convert.ToInt32(Console.ReadLine());
+        
+            if(a > b)
+            {
+                Console.WriteLine("El mayor es: " + a);
+            }
+            else
+            {
+                Console.WriteLine("El mayor es: " + b);
+
+            }
+        }
+    }
+}
